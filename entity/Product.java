403019687912ae7856358ba5a980016e84b2c435
Buse Keklik <busekeklik23@gmail.com>
2024-05-08 15:27:01 +0300

    entity added

---
+++ entity/Product.java
@@ -0,0 +1,38 @@
+package entity;
+
+public class Product {
+    long productId;
+    String productName;
+    double salesPrice;
+
+    public Product(long productId, String productName, double salesPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.salesPrice = salesPrice;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+}
