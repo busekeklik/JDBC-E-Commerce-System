3bd9881d15d228debae198ed5797f8879eb2c3b8
Buse Keklik <busekeklik23@gmail.com>
2024-05-09 16:08:20 +0300

    ProductDeleteTest added

--- EMPTest/ProductDeleteTest.java
+++ EMPTest/ProductDeleteTest.java
@@ -5,10 +5,12 @@ import manager.ProductManager;
 import java.sql.SQLException;
 
 public class ProductDeleteTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         ProductManager productManager = new ProductManager();
         Product product = new Product();
 
-        boolean deleted = productManager.delete(product);
+        boolean deleted = productManager.delete(1);
+        System.out.println(deleted ? "Product deleted" : "Product not deleted");
+        
     }
 }
